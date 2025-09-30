5561f537cfd203d3dffe50e8bd2d04b6d8f3c1a6
Pozdrawiam <witam@tuta.io>
2025-09-30 21:34:26 +0200

    Add DI setup for WordsRepository in Core and WebApp
    Introduces DependencyInjection.cs in Otw.Core to register IWordsRepository as a singleton. Updates Otw.Core.csproj to include Microsoft.Extensions.DependencyInjection.Abstractions. Integrates AddInfrastructure extension in Otw.WebApp Program.cs to enable DI for infrastructure services.

---
+++ src/Otw.Core/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Otw.Core.Domain;
+using Otw.Core.Infrastructure;
+
+namespace Otw.Core;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddSingleton<IWordsRepository, WordsRepository>();
+        
+        return services;
+    }
+}
--- src/Otw.WebApp/Program.cs
+++ src/Otw.WebApp/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Otw.Core;
 using Otw.WebApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddInfrastructure();
 
 await builder.Build().RunAsync();
