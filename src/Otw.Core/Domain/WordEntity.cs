bb329103d9f019567a2940cc3ac2d4a75637b2a2
Pozdrawiam <witam@tuta.io>
2025-09-30 21:18:14 +0200

    Rename Index property to Id in WordEntity
    The property 'Index' in the WordEntity class has been renamed to 'Id' to better reflect its purpose as a unique identifier.

--- src/Otw.Core/Domain/WordEntity.cs
+++ src/Otw.Core/Domain/WordEntity.cs
@@ -2,7 +2,7 @@
 
 public class WordEntity
 {
-    public required int Index { get; init; }
+    public required int Id { get; init; }
     public required string Value { get; init; }
     public required string Translation { get; set; }
 }
