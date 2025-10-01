bb5d9d3609cb5f2851c0bbc3c7b43af2c6fc6ba8
Pozdrawiam <witam@tuta.io>
2025-10-01 20:26:53 +0200

    Register NextWordCmdHandler in DI container
    Added NextWordCmdHandler to the scoped services in the dependency injection setup to ensure it is available for injection throughout the application.

--- src/Otw.Core/DependencyInjection.cs
+++ src/Otw.Core/DependencyInjection.cs
@@ -9,6 +9,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddDomain(this IServiceCollection services)
     {
+        services.AddScoped<NextWordCmdHandler>();
         services.AddScoped<LastWordQueryHandler>();
         
         return services;
