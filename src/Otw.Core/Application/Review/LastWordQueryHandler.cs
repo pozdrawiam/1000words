18f16a693df291e7545dbbc5c574c93029e17c56
Pozdrawiam <witam@tuta.io>
2025-10-03 21:39:14 +0200

    Review mode

---
+++ src/Otw.Core/Application/Review/LastWordQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using Otw.Core.Domain;
+
+namespace Otw.Core.Application.Review;
+
+public class LastWordQueryHandler
+{
+    private readonly ILocalStorageService _localStorage;
+    private readonly IWordsRepository _repo;
+
+    public LastWordQueryHandler(ILocalStorageService localStorage, IWordsRepository repo)
+    {
+        _localStorage = localStorage;
+        _repo = repo;
+    }
+    
+    public async Task<WordEntity> ExecuteAsync()
+    {
+        int lastWordId = 1;
+        var storedId = await _localStorage.GetItemAsync("Review_lastWordId");
+        
+        if (!string.IsNullOrEmpty(storedId) && int.TryParse(storedId, out var parsedId))
+            lastWordId = parsedId;
+
+        var lastWord = await _repo.GetByIdAsync(lastWordId);
+
+        if (lastWord is not null)
+            return lastWord;
+        
+        var firstWord = (await _repo.GetAllAsync()).First();
+        return firstWord;
+    }
+}
---
+++ src/Otw.Core/Application/Review/NextWordCmdHandler.cs
@@ -0,0 +1,33 @@
+﻿using Otw.Core.Domain;
+
+namespace Otw.Core.Application.Review;
+
+public class NextWordCmdHandler
+{
+    private readonly ILocalStorageService _localStorage;
+    private readonly IWordsRepository _repo;
+
+    public NextWordCmdHandler(ILocalStorageService localStorage, IWordsRepository repo)
+    {
+        _localStorage = localStorage;
+        _repo = repo;
+    }
+
+    public async Task<WordEntity> ExecuteAsync(int currentWordId)
+    {
+        var nextWordId = currentWordId + 1;
+        var nextWord = await _repo.GetByIdAsync(nextWordId);
+
+        if (nextWord is not null)
+        {
+            await _localStorage.SetItemAsync("Review_lastWordId", nextWord.Id.ToString());
+            
+            return nextWord;
+        }
+        
+        var firstWord = (await _repo.GetAllAsync()).First();
+        await _localStorage.SetItemAsync("Review_lastWordId", firstWord.Id.ToString());
+        
+        return firstWord;
+    }
+}
