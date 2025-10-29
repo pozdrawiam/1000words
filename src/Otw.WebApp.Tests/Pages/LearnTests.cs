72a42e2b43d7a6fd628bbbfc19128436297e1765
pozdrawiam <pozdrawiam@pozdrawiam>
2025-10-29 20:27:02 +0100

    Add test for rendering next word in Learn.razor page

--- src/Otw.WebApp.Tests/Pages/LearnTests.cs
+++ src/Otw.WebApp.Tests/Pages/LearnTests.cs
@@ -1,4 +1,5 @@
 using Bunit;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using Otw.Core.Application.Learn;
@@ -18,7 +19,7 @@ public class LearnTests : TestContext
     }
     
     [Fact]
-    public void Should_RenderWord()
+    public void Should_RenderCurrentWord()
     {
         _lastWOrdQueryHandler.Setup(x => x.ExecuteAsync())
             .ReturnsAsync(new WordEntity
@@ -32,5 +33,38 @@ public class LearnTests : TestContext
         var cut = RenderComponent<Otw.WebApp.Pages.Learn>();
         
         Assert.Contains("Test1", cut.Markup);
+    }
+
+    [Fact]
+    public async Task Should_RenderNextWord()
+    {
+        var firstWord = new WordEntity
+        {
+            Id = 1,
+            Value = "Test1",
+            Translation = "Test1Translation"
+        };
+        var nextWord = new WordEntity
+        {
+            Id = 2,
+            Value = "Test2",
+            Translation = "Test2Translation"
+        };
+
+        _lastWOrdQueryHandler
+            .Setup(x => x.ExecuteAsync())
+            .ReturnsAsync(firstWord);
+
+        _nextWordCmdHandler
+            .Setup(x => x.ExecuteAsync(firstWord.Id))
+            .ReturnsAsync(nextWord);
+
+        var cut = RenderComponent<Otw.WebApp.Pages.Learn>();
+        var button = cut.Find("button.btn-primary");
+        
+        // Act
+        await button.ClickAsync(new MouseEventArgs());
+        
+        Assert.Contains("Test2", cut.Markup);
     }
 }
