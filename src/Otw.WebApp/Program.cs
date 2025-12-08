360d27c9c7dae30a064cb1d6a35b6e59b1e4c206
Pozdrawiam <witam@tuta.io>
2025-12-08 21:28:42 +0100

    Add localization support to Otw.WebApp with necessary service and package updates

--- src/Otw.WebApp/Program.cs
+++ src/Otw.WebApp/Program.cs
@@ -7,6 +7,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddLocalization();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddDomain();
