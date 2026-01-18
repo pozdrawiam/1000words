0124b259a0e534fdc9bcb8692a94d21897b38875
Pozdrawiam <witam@tuta.io>
2026-01-18 16:32:23 +0100

    Add handlers for learning sort type settings
    Introduces LearnSortTypeQueryHandler and SetLearSortTypeCmdHandler interfaces and classes to manage retrieval and updating of WordSortType settings. Implementations are currently placeholders with NotImplementedException.

---
+++ src/Otw.Core/Application/Settings/LearnSortTypeQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using Otw.Core.Domain;
+
+namespace Otw.Core.Application.Settings;
+
+public interface ILearnSortTypeQueryHandler
+{
+    Task<WordSortType> ExecuteAsync();
+}
+
+public class LearnSortTypeQueryHandler : ILearnSortTypeQueryHandler
+{
+    public Task<WordSortType> ExecuteAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Otw.Core/Application/Settings/SetLearSortTypeCmdHandler.cs
@@ -0,0 +1,16 @@
+﻿using Otw.Core.Domain;
+
+namespace Otw.Core.Application.Settings;
+
+public interface ISetLearSortTypeCmdHandler
+{
+    Task ExecuteAsync(WordSortType sortType);
+}
+
+public class SetLearSortTypeCmdHandler : ISetLearSortTypeCmdHandler
+{
+    public Task ExecuteAsync(WordSortType sortType)
+    {
+        throw new NotImplementedException();
+    }
+}
