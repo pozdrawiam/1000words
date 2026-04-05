715dc9251b9ec303798aa039052cbe42665e1ae9
Pozdrawiam <witam@tuta.io>
2026-04-05 19:06:56 +0200

    Refactor MoveReviewPrevWordCmdHandler to improve previous word retrieval logic and simplify execution flow

--- src/Otw.Core/Application/Review/MoveReviewPrevWordCmdHandler.cs
+++ src/Otw.Core/Application/Review/MoveReviewPrevWordCmdHandler.cs
@@ -20,19 +20,24 @@ public class MoveReviewPrevWordCmdHandler : IMoveReviewPrevWordCmdHandler
 
     public async Task<WordEntity> ExecuteAsync(int currentWordId)
     {
-        var previousWordId = currentWordId - 1;
-        var previousWord = await _repo.GetByIdAsync(previousWordId);
+        var sortType = await _parameters.GetReviewSortTypeAsync();
+        var previousWord = await GetPreviousWordAsync(currentWordId, sortType);
+        
+        await _parameters.SetReviewLastWordIdAsync(previousWord.Id);
+        
+        return previousWord;
+    }
+    
+    private async Task<WordEntity> GetPreviousWordAsync(int currentWordId, WordSortType sortType)
+    {
+        var words = (await WordsHelper.GetWordsSortedAsync(_repo, sortType)).ToList();
+        var currentWordIndex = words.FindIndex(w => w.Id == currentWordId);
 
-        if (previousWord is not null)
-        {
-            await _parameters.SetReviewLastWordIdAsync(previousWord.Id);
-            
-            return previousWord;
-        }
+        if (currentWordIndex == -1)
+            return words.First();
         
-        var firstWord = (await _repo.GetAllAsync()).First();
-        await _parameters.SetReviewLastWordIdAsync(firstWord.Id);
+        var previousWordIndex = (currentWordIndex - 1) % words.Count;
         
-        return firstWord;
+        return previousWordIndex < 0 ? words.Last() : words[previousWordIndex];
     }
 }
